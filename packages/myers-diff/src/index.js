18eba47ceec5ed3d2a7d91a45b3a909259cbccca
moudev <rigobertomauriciomartinez@gmail.com>
2022-03-06 16:34:42 -0600

    feat: add main classes to myers exports

--- packages/myers-diff/src/index.js
+++ packages/myers-diff/src/index.js
@@ -1,9 +1,15 @@
-const { diff } = require('./myers');
+const {
+  Myers,
+  Myers: { diff },
+  Encoder
+} = require('./myers');
 const formats = require('./formats');
 const changed = require('./changed');
 
 // to use destructuring in browser version
 // https://rollupjs.org/guide/en/#outputexports
+exports.Myers = Myers
+exports.Encoder = Encoder
 exports.diff = diff
 exports.formats = formats
 exports.changed = changed
--- packages/myers-diff/src/myers.js
+++ packages/myers-diff/src/myers.js
@@ -355,4 +355,4 @@ class Myers {
 	}
 }
 
-module.exports = Myers;
+module.exports = { Myers, Encoder };

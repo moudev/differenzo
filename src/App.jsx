d1cc4ff9ea199a3d790529df7a4f1388ccab852a
moudev <rigobertomauriciomartinez@gmail.com>
2022-03-16 21:57:12 -0600

    feat: add save quotes in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import short from 'short-uuid'
 
 import './App.css'
 
 import Card from './components/Card'
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const tasksFromLocalStorage = localStorage.getItem('items')
+      ? localStorage.getItem('items')
+      : '[]'
+
+    setTasks(JSON.parse(tasksFromLocalStorage))
+  }, [])
+
+  const handleTask = (data, task) => {
+    if (!task) {
+      const item = {
+        id: short.uuid(),
+        original: {
+          ...data.original,
+        },
+        modified: {
+          ...data.modified,
+        },
+      }
+      setTasks((prevTasks) => {
+        const newTasks = [item, ...prevTasks]
+        localStorage.setItem('items', JSON.stringify(newTasks))
+        return newTasks
+      })
+    } else if (task && task.id) {
+      setTasks((prevTasks) => {
+        const index = prevTasks.findIndex((element) => element.id === task.id)
+
+        const item = {
+          ...prevTasks[index],
+          original: data.original,
+          modified: data.modified,
+        }
+
+        prevTasks.splice(index, 1, item)
+
+        const newTasks = [...prevTasks]
+        localStorage.setItem('items', JSON.stringify(newTasks))
+        return newTasks
+      })
+    }
+  }
+
   return (
     <div className="bg-$bg-primary text-$color-primary font-semibold min-h-screen">
       <div className="container mx-auto">
         <div className="px-6 py-8 grid gap-y-16 md:px-8 lg:py-12">
-          <Card />
+          <Card handleTask={handleTask} />
+          {tasks.map((task) => (
+            <Card task={task} key={task.id} handleTask={handleTask} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-underscore-dangle */
-import React, { useState, useRef } from 'react'
+import React, { useRef } from 'react'
 import { Myers, Encoder } from '@moudev/myers-diff'
 import _debounce from 'lodash/debounce'
 
-function Card() {
+function Card({ task, handleTask }) {
   const originalRef = useRef()
   const modifiedRef = useRef()
-  const [results, setResults] = useState([])
   const encoder = new Encoder()
 
   const compareTexts = () => {
@@ -38,11 +38,24 @@ function Card() {
       modifiedEncoded.length
     )
 
-    setResults([originalEncoded, modifiedEncoded])
+    handleTask(
+      {
+        original: {
+          fullText: original,
+          ...originalEncoded,
+        },
+        modified: {
+          fullText: modified,
+          ...modifiedEncoded,
+        },
+      },
+      task
+    )
   }
 
   // https://medium.com/nerd-for-tech/debounce-your-search-react-input-optimization-fd270a8042b
   const handleInput = _debounce(compareTexts, 500)
+
   return (
     <div className="border border-dashed border-purple-400 p-4 pb-6 rounded-lg grid lg:(grid-cols-2 gap-x-12 pb-6)">
       <div className="grid gap-4 lg:content-start">
@@ -51,25 +64,27 @@ function Card() {
           className="p-2 rounded-lg min-h-48 md:min-h-40 focus:outline-dotted-purple-400 outline-2 outline-offset-4 lg:text-lg"
           onChange={handleInput}
           ref={originalRef}
+          /* value={task?.original?.fullText ? task.original.fullText : ''} */
         />
         <h3 className="text-$color-secondary text-center">Modified</h3>
         <textarea
           className="p-2 rounded-lg min-h-48 md:min-h-40 focus:outline-dotted-purple-400 outline-2 outline-offset-4 lg:text-lg"
           onChange={handleInput}
           ref={modifiedRef}
+          /* value={task?.modified?.fullText ? task.modified.fullText : ''} */
         />
       </div>
-      {results.length === 2 && (
+      {task && task.id && (
         <div className="grid gap-y-4 mt-12 pb-2 md:pb-0 lg:(content-start mt-0)">
           <h3 className="text-$color-secondary text-center hidden lg:inline-block ">
             Differences
           </h3>
           <div className="bg-red-200 p-2 rounded-lg flex flex-wrap content-start gap-y-3 md:p-3">
-            {results[0]._parts.map((m, index) => (
+            {task.original._parts.map((m, index) => (
               <span
                 key={`${m.text}-${m.pos}`}
                 className={`mr-2 p-1 rounded-md ${
-                  results[0]._modified[`${index}`] ? 'bg-red-300' : ''
+                  task.original._modified[`${index}`] ? 'bg-red-300' : ''
                 }`}
               >
                 {m.text}
@@ -77,19 +92,17 @@ function Card() {
             ))}
           </div>
           <div className="bg-green-200 p-2 rounded-lg flex flex-wrap content-start gap-y-3 md:p-3">
-            {results[1]._parts.map((m, index) => (
+            {task.modified._parts.map((m, index) => (
               <span
                 key={`${m.text}-${m.pos}`}
                 className={`mr-2 p-1 rounded-md ${
-                  results[1]._modified[`${index}`] ? 'bg-green-300' : ''
+                  task.modified._modified[`${index}`] ? 'bg-green-300' : ''
                 }`}
               >
                 {m.text}
               </span>
             ))}
           </div>
-
-          <p />
         </div>
       )}
     </div>
